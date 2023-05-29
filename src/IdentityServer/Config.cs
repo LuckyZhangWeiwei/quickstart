302a93fe1be4f98021bb3503bb3745f8515057ba
LuckyZhangWeiwei <zhangzhang_10@hotmail>
2023-05-29 14:01:33 +0800

    init

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -1,4 +1,6 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
 
 namespace IdentityServer;
 
@@ -7,7 +9,17 @@ public static class Config
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
         {
-            new IdentityResources.OpenId()
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource()
+            {
+                  Name = "verification",
+                  UserClaims = new List<string>
+                  {
+                      JwtClaimTypes.Email,
+                      JwtClaimTypes.EmailVerified
+                  }
+            }
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -17,23 +29,39 @@ public static class Config
             };
 
     public static IEnumerable<Client> Clients =>
-        new Client[]
+    new List<Client>
+    {
+        // machine to machine client (from quickstart 1)
+        new Client
+        {
+            ClientId = "client",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            // scopes that client has access to
+            AllowedScopes = { "api1" }
+        },
+        // interactive ASP.NET Core Web App
+        new Client
+        {
+            ClientId = "web",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.Code,
+            
+            // where to redirect to after login
+            RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+            AllowedScopes = new List<string>
             {
-             new Client
-                {
-                    ClientId = "client",
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                "verification"
+            }
+        }
+    };
 
-                    // no interactive user, use the clientid/secret for authentication
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-
-                    // secret for authentication
-                    ClientSecrets =
-                    {
-                        new Secret("secret".Sha256())
-                    },
-
-                    // scopes that client has access to
-                    AllowedScopes = { "api1" }
-                }
-            };
 }
