309b7c519aeb4b250a1a3f49d212f3f306ea3bf7
LuckyZhangWeiwei <zhangzhang_10@hotmail>
2023-05-30 20:07:35 +0800

    change

--- JavascriptClient/HostingExtensions.cs
+++ JavascriptClient/HostingExtensions.cs
@@ -1,74 +1,96 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿//using Duende.Bff.Yarp;
+//using System.IdentityModel.Tokens.Jwt;
 
-namespace JavascriptClient
-{
-    internal static class HostingExtensions
-    {
-        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
-        {
-            var services = builder.Services;
+//namespace JavascriptClient
+//{
+//    internal static class HostingExtensions
+//    {
+//        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+//        {
+//            var services = builder.Services;
 
-            services.AddControllers();
-            services.AddAuthorization();
+//            services.AddControllers();
+//            services.AddAuthorization();
 
-            services.AddBff();
+//            services.AddBff().AddRemoteApis();
 
-            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+//            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = "Cookies";
-                options.DefaultChallengeScheme = "oidc";
-                options.DefaultSignOutScheme = "oidc";
-            })
-            .AddCookie("Cookies")
-            .AddOpenIdConnect("oidc", options =>
-            {
-                options.Authority = "https://localhost:5001";
+//            services.AddAuthentication(options =>
+//            {
+//                options.DefaultScheme = "Cookies";
+//                options.DefaultChallengeScheme = "oidc";
+//                options.DefaultSignOutScheme = "oidc";
+//            })
+//            .AddCookie("Cookies")
+//            .AddOpenIdConnect("oidc", options =>
+//            {
+//                options.Authority = "https://localhost:5001";
 
-                options.ClientId = "bff";
-                options.ClientSecret = "secret";
-                options.ResponseType = "code";
+//                options.ClientId = "bff";
+//                options.ClientSecret = "secret";
+//                options.ResponseType = "code";
 
-                options.Scope.Add("api1");
+//                options.Scope.Add("api1");
 
-                options.SaveTokens = true;
-                options.GetClaimsFromUserInfoEndpoint = true;
-            });
+//                options.SaveTokens = true;
+//                options.GetClaimsFromUserInfoEndpoint = true;
+//            });
 
-            return builder.Build();
-        }
+//            return builder.Build();
+//        }
 
-        public static WebApplication ConfigurePipeline(this WebApplication app)
-        {
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+//        public static WebApplication ConfigurePipeline(this WebApplication app)
+//        {
+//            if (app.Environment.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//            }
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+//            app.UseDefaultFiles();
+//            app.UseStaticFiles();
 
-            app.UseRouting();
-            app.UseAuthentication();
+//            app.UseRouting();
+//            app.UseAuthentication();
 
-            app.UseBff();
+//            app.UseBff();
 
-            app.UseAuthorization();
+//            app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers()
-                    .AsBffApiEndpoint();
+//            //app.UseEndpoints(endpoints =>
+//            //{
+//            //    endpoints.MapControllers()
+//            //        .AsBffApiEndpoint();
 
-                endpoints.MapBffManagementEndpoints();
+//            //    endpoints.MapBffManagementEndpoints();
 
-                endpoints.MapRemoteBffApiEndpoint("/remote", "https://localhost:6001")
-                    .RequireAccessToken(Duende.Bff.TokenType.User);
+//            //    endpoints.MapRemoteBffApiEndpoint("/remote", "https://localhost:6001")
+//            //        .RequireAccessToken(Duende.Bff.TokenType.User);
 
-            });
+//            //});
 
-            return app;
-        }
-    }
-}
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapBffManagementEndpoints();
+//                // Uncomment this for Controller support
+//                // endpoints.MapControllers()
+//                //     .AsBffApiEndpoint();
+//                endpoints.MapGet("/local/identity", LocalIdentityHandler)
+//                    .AsBffApiEndpoint();
+//                endpoints.MapRemoteBffApiEndpoint("/remote", "https://localhost:6001")
+//                    .RequireAccessToken(Duende.Bff.TokenType.User);
+//            });
+
+
+
+//            return app;
+//        }
+
+//        [Authorize]
+//        static IResult LocalIdentityHandler(ClaimsPrincipal user, HttpContext context)
+//        {
+//            var name = user.FindFirst("name")?.Value ?? user.FindFirst("sub")?.Value;
+//            return Results.Json(new { message = "Local API Success!", user = name });
+//        }
+//    }
+//}
